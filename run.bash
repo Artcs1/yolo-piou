1b0017e4c08e62c46dc6f3a4097baa37032218b9
Artcs1 <ju.jeffri.v@gmail.com>
2021-02-08 23:53:37 -0300

    minor modified

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 train.py --data voc2007.yaml --weights '' --batch-size 16 --epochs 641 --cfg yolov3-spp.yaml
+python -m torch.distributed.launch --nproc_per_node 2 python3 train.py --data voc2007.yaml --weights '' --batch-size 16 --epochs 641 --cfg yolov3.yaml
